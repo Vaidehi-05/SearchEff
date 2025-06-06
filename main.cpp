b8376edb11fbd56600e43bf2fe4e75b754eb5d55
Vaidehi-05 <siasrivastava14@gmail.com>
2025-06-06 18:39:51 +0530

    Initial features created!:

--- main.cpp
+++ main.cpp
@@ -1,1 +1,118 @@
+#include <bits/stdc++.h>
+#include <filesystem>
+using namespace std;
+namespace fs=std::filesystem;
+
+void displaySet(set<pair<string, string>> s)
+{
+    for(auto it=s.begin(); it!=s.end(); it++)
+    {
+        cout<<it->first<<endl;
+        cout<<it->second<<endl;
+    }
+}
+class searchEff{
+public:
+    map<string, map<int, string>> unimap;
+    set <pair<string,string>> todos;
+    set <pair<string,string>> ideas;
+    set <pair<string,string>> torevise;
+    set <string> allUniqueWords;
+    void readFile(string filename)
+    {
+        ifstream file(filename);
+        if(!file.is_open())
+            cout<<"Error! Could not open file."<<endl;
+        map<string, int> uniqWrds;
+        string line="";
+        while(getline(file, line))
+        {
+            transform(line.begin(), line.end(), line.begin(), ::tolower);
+           // cout<<line<<endl;
+            int l=line.size(), i=0;
+            while(line[i]==' '&&i<l)
+                i+=1;
+            if(i==l)
+                continue;
+            if(line.find("@to-do")!=string::npos)
+            {
+                todos.insert({line.substr(i+6), filename});
+                continue;
+            }
+            if(line.find("::idea")!=string::npos)
+            {
+                ideas.insert({line.substr(i+6), filename});
+                continue;
+            }
+            if(line.find("@to-revise")!=string::npos)
+            {
+                torevise.insert({line.substr(i+10), filename});
+                continue;
+            }
+            line.push_back(' ');
+            int j=i,len=line.size();
+            string word="";
+            while(j<len)
+            {
+                if(line[j]==' ')
+                {
+                    if(uniqWrds.find(word)!=uniqWrds.end())
+                        uniqWrds[word]+=1;
+                    else
+                        uniqWrds[word]=1;
+                    word="";
+                }
+                else
+                word+=line[j];
+                j+=1;
+            }
+            if(j==len)
+            {
+                for(auto it=uniqWrds.begin(); it!=uniqWrds.end(); it++)
+                {
+                    unimap[it->first].insert({it->second, filename});
+                }
+            }
+        }
+    }
+    void readAllFiles(string foldername)
+    {
+        for (auto entry : fs::directory_iterator(foldername)) {
+            if (fs::is_regular_file(entry) && entry.path().extension() == ".txt") {
+                string str=entry.path().string();
+               // cout << "Found file: " << str<< "\n";
+                readFile(str);
+            }
+        }
+    }
+    void displayUniMap()
+    {
+        cout<<"Displaying UniMap: ";
+        for(auto it=unimap.begin(); it!=unimap.end(); it++)
+        {
+            cout<<it->first<<endl;
+            for(auto itr=it->second.begin(); itr!=it->second.end(); itr++)
+            {
+                cout<<itr->first<<"  "<<itr->second<<endl;
+            }
+            cout<<endl;
+        }
+    }
+    };
+int main()
+{
+    searchEff obj;
+    obj.readAllFiles("C:/Users/Sia Srivastava/OneDrive/Desktop/Git folder/SearchEff/pqrs");
+    cout<<"TO-DOS"<<endl;
+    displaySet(obj.todos);
+    cout<<"IDEAS"<<endl;
+    displaySet(obj.ideas);
+    cout<<"TO REVISE"<<endl;
+    displaySet(obj.torevise);
+    obj.displayUniMap();
+    return 0;
+}
+
+
+
 
---
+++ pqrs/third.md
@@ -0,0 +1,13 @@
+#dynamic-programming
+Dynamic programming is about breaking problems into overlapping subproblems.
+
+We must memoize or tabulate to avoid recomputation.
+
+::idea Build a visual representation of recursion tree flattening
+
+@to-do Revise longest increasing subsequence problem
+
+#graph-theory
+Understand Dijkstra's and Floyd-Warshall algorithms.
+
+@to-revise Practice converting adjacency matrix to adjacency list
